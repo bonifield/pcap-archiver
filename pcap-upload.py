b42ea76d9f726b0e56352e2010a9f3986a1a2628
bonifield <bonifield@users.noreply.github.com>
2020-10-14 14:02:44 -0500

    Update pcap-upload.py

--- pcap-upload.py
+++ pcap-upload.py
@@ -2,7 +2,7 @@
 
 
 #
-# last updated: 2020-09-23
+# last updated: 2020-10-13
 # example usage:
 #	pcap-uploader.py file.pcap
 #	- ideally use with a cronjob that looks for new pcaps and invokes this script
@@ -20,6 +20,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 # all Scapy features
 from scapy.all import *
+load_layer("http")
+load_layer("tls")
 
 
 #============
@@ -34,7 +36,6 @@ api = 'YOUR-B64-PUBLISH-KEY-HERE=='
 
 
 url = host+uri
-starttime = time.time()
 inputFile = sys.argv[1]
 epoch = str(int(time.time()))
 outputFile = "pcapindex.out."+epoch+".log"
@@ -48,10 +49,11 @@ heady = {
 }
 
 
-def indexer(pkt):
+def processPacket(pkt):
 	d = {
 		"destination":{
 			"address":"",
+			"domain":"",
 			"ip":"",
 			"port":"",
 			"mac":""
@@ -59,10 +61,34 @@ def indexer(pkt):
 		"file":{
 			"name":""
 		},
+		"http":{
+			"header":{
+				"content_type":"",
+				"forwarded":"",
+				"location":"",
+				"status_code_phrase":"",
+				"server":"",
+				"via":"",
+				"x-forwarded-for":"",
+				"x-forwarded-host":"",
+				"x-forwarded-proto":"",
+				"x-powered-by":""
+			},
+			"request": {
+				"method": "",
+				"referrer": ""
+			},
+			"response": {
+				"status_code":""
+			}
+		},
 		"icmp":{
 			"code":"",
 			"type":""
 		},
+		"is_http": "",
+		"is_icmp": "",
+		"is_tls": "",
 		"is_vlan": "",
 		"network":{
 			"transport":"",
@@ -81,6 +107,18 @@ def indexer(pkt):
 			"ip":"",
 			"port":"",
 			"mac":""
+		},
+		"tls":{
+			"client":{
+				"server_name":""
+			}
+		},
+		"url":{
+			"domain":"",
+			"path":""
+		},
+		"user_agent":{
+			"original":""
 		}
 	}
 	d['file']['name'] = inputFile
@@ -103,10 +141,10 @@ def indexer(pkt):
 			d['network']['version'] = str(pkt[IP].version)
 			d['network']['transport'] = str(pkt[IP].proto)
 		if Dot1Q in pkt:
+			d['is_vlan'] = 'yes'
 			nt = pkt[Dot1Q].type
 			d['network']['type'] = str(etherTypeNames[nt]).lower()
 			d['network']['vlan']['id'] = pkt[Dot1Q].vlan
-			d['is_vlan'] = 'yes'
 		if TCP in pkt:
 			d['source']['port'] = str(pkt[TCP].sport)
 			d['destination']['port'] = str(pkt[TCP].dport)
@@ -114,8 +152,65 @@ def indexer(pkt):
 			d['source']['port'] = str(pkt[UDP].sport)
 			d['destination']['port'] = str(pkt[UDP].dport)
 		if ICMP in pkt:
+			d['is_icmp'] = 'yes'
 			d['icmp']['type'] = str(pkt[ICMP].type)
 			d['icmp']['code'] = str(pkt[ICMP].code)
+		if HTTP in pkt:
+			# https://scapy.readthedocs.io/en/latest/api/scapy.layers.http.html
+			d['is_http'] = 'yes'
+			if HTTPRequest in pkt[HTTP]:
+				hreq = pkt[HTTP][HTTPRequest]
+				if hreq.Method:
+					d['http']['request']['method'] = bytes(hreq.Method).decode("utf-8")
+				if hreq.Host:
+					d['url']['domain'] = bytes(hreq.Host).decode("utf-8")
+					d['destination']['domain'] = bytes(hreq.Host).decode("utf-8")
+				if hreq.Path:
+					d['url']['path'] = bytes(hreq.Path).decode("utf-8")
+				if hreq.Http_Version:
+					d['http']['version'] = bytes(hreq.Http_Version).decode("utf-8")
+				if hreq.User_Agent:
+					d['user_agent']['original'] = bytes(hreq.User_Agent).decode("utf-8")
+				if hreq.Content_Type:
+					d['http']['header']['content_type'] = bytes(hreq.Content_Type).decode("utf-8")
+				if hreq.Referer:
+					d['http']['request']['referrer'] = bytes(hreq.Referer).decode("utf-8")
+				if hreq.Via:
+					d['http']['header']['via'] = bytes(hreq.Via).decode("utf-8")
+				if hreq.Forwarded:
+					d['http']['header']['forwarded'] = bytes(hreq.Forwarded).decode("utf-8")
+				if hreq.X_Forwarded_For:
+					d['http']['header']['x-forwarded-for'] = bytes(hreq.X-Forwarded-For).decode("utf-8")
+				if hreq.X_Forwarded_Host:
+					d['http']['header']['x-forwarded-host'] = bytes(hreq.X-Forwarded-Host).decode("utf-8")
+				if hreq.X_Forwarded_Proto:
+					d['http']['header']['x-forwarded-proto'] = bytes(hreq.X-Forwarded-Proto).decode("utf-8")
+			if HTTPResponse in pkt[HTTP]:
+				hres = pkt[HTTP][HTTPResponse]
+				if hres.Status_Code:
+					d['http']['response']['status_code'] = bytes(hres.Status_Code).decode("utf-8")
+				if hres.Reason_Phrase:
+					d['http']['header']['status_code_phrase'] = bytes(hres.Reason_Phrase).decode("utf-8")
+				if hres.Content_Type:
+					d['http']['header'][''] = bytes(hres.Content_Type).decode("utf-8")
+				if hres.Server:
+					d['http']['header']['server'] = bytes(hres.Server).decode("utf-8")
+				if hres.Http_Version:
+					d['http']['version'] = bytes(hres.Http_Version).decode("utf-8")
+				if hres.Via:
+					d['http']['header']['via'] = bytes(hres.Via).decode("utf-8")
+				if hres.X_Powered_By:
+					d['http']['header']['x-powered-by'] = bytes(hres.X-Forwarded-Proto).decode("utf-8")
+		if TLS in pkt:
+			d['is_tls'] = 'yes'
+			m = pkt[TLS].msg[0]
+			try:
+				snb = m[TLS_Ext_ServerName].servernames[0].servername
+				sn = bytes(snb).decode("utf-8")
+				d['tls']['client']['server_name'] = sn
+				d['destination']['domain'] = sn
+			except Exception as e:
+				pass
 		# reverse with base64_bytes(z.lstrip("b'").rstrip("'")) when re-combining into pcap from database retrieval
 		b = str(bytes_base64(pkt))
 		d['packet']['encoded'] = b
@@ -133,56 +228,64 @@ def indexer(pkt):
 			pass
 
 
-with PcapReader(inputFile) as pr:
-	oFileList = [] # store list of temp filenames
-	c = 0 # count number of loops
-	fnameCounter = 0 # counter appended to temporary log names
-	oFileName = outputFile+'.'+str(fnameCounter)
-	print("opening {}".format(oFileName))
-	oFile = open(oFileName, 'w')
-	oFileList.append(oFileName)
-	for pkt in pr:
-		# cap files created at 20k loops
-		if c >= 20000: # this would be 40k lines created by indexer()
-			print("closing {}".format(oFileName))
-			oFile.close()
-			c = 0
-			fnameCounter += 1
-			oFileName = outputFile+'.'+str(fnameCounter)
-			print("opening {}".format(oFileName))
-			oFile = open(oFileName, 'w')
-			oFileList.append(oFileName)
+def makeWorkingFiles(inputFile):
+	with PcapReader(inputFile) as pr:
+		oFileList = [] # store list of temp filenames
+		c = 0 # count number of loops
+		fnameCounter = 0 # counter appended to temporary log names
+		oFileName = outputFile+'.'+str(fnameCounter)
+		print("opening {}".format(oFileName))
+		oFile = open(oFileName, 'w')
+		oFileList.append(oFileName)
+		for pkt in pr:
+			# cap files created at 20k loops
+			if c >= 20000: # this would be 40k lines created by processPacket()
+				print("closing {}".format(oFileName))
+				oFile.close()
+				c = 0
+				fnameCounter += 1
+				oFileName = outputFile+'.'+str(fnameCounter)
+				print("opening {}".format(oFileName))
+				oFile = open(oFileName, 'w')
+				oFileList.append(oFileName)
+			try:
+				# processPacket() only attempts to process packets with Ethernet
+				oFile.write(processPacket(pkt))
+			except Exception as e:
+				pass
+			# increment number of loops
+			c += 1
+		oFile.write('\n')
+		print("closing {}".format(oFileName))
+		oFile.close()
+	pr.close()
+	print("made temp files, preparing to upload")
+	return(oFileList)
+
+
+def uploadWorkingFiles(oFileList):
+	# read list of filenames
+	for o in oFileList:
 		try:
-			# indexer only attempts to process packets with Ethernet
-			oFile.write(indexer(pkt))
+			with open(o, 'rb') as f:
+				print("uploading {}".format(o))
+				r = requests.post(url, headers=heady, verify=False, data=f)
+				# un-comment to see results, VERY loud
+				#c = r.content
+				#print(json.dumps(json.loads(c), indent=4))
+			f.close()
+			# delete the temporary log file
+			print("removing {}".format(o))
+			os.remove(o)
 		except Exception as e:
-			pass
-		# increment number of loops
-		c += 1
-	oFile.write('\n')
-print("closing {}".format(oFileName))
-oFile.close()
-print("made temp files, preparing to upload")
+			print("error uploading file {}".format(o))
+			print(str(e))
 
 
-# read list of filenames
-for o in oFileList:
-	try:
-		with open(o, 'rb') as f:
-			print("uploading {}".format(o))
-			r = requests.post(url, headers=heady, verify=False, data=f)
-			# un-comment to see results, VERY loud
-			#c = r.content
-			#print(json.dumps(json.loads(c), indent=4))
-		f.close()
-		# delete the temporary log file
-		print("removing {}".format(o))
-		os.remove(o)
-	except Exception as e:
-		print("error uploading file {}".format(o))
-		print(str(e))
-
-
-endtime = time.time()
-fintime = str(int(endtime-starttime))
-print("script finished in {} seconds".format(fintime))
+if __name__ == '__main__':
+	starttime = time.time()
+	fileListToUpload = makeWorkingFiles(inputFile)
+	uploadWorkingFiles(fileListToUpload)
+	endtime = time.time()
+	fintime = str(int(endtime-starttime))
+	print("script finished in {} seconds".format(fintime))
